2cf6a50fc7d5c43bf744b4ce87346cffb1362eb6
Ciro Monteiro <cirovisqui@gmail.com>
2025-10-30 20:52:34 -0300

    adicionando gráfico 'area' para traduzir o 'insight' sobre receita mensal

--- graphs.py
+++ graphs.py
@@ -1,6 +1,5 @@
 import plotly.express as px
-from dataset import df_rec_estado
-
+from dataset import df_rec_estado,df_rec_mensal
 grafico_map_estado = px.scatter_geo(
     data_frame=df_rec_estado,
     lat= 'lat',
@@ -11,4 +10,12 @@ grafico_map_estado = px.scatter_geo(
     hover_name='Local da compra',
     hover_data={'lat': False, 'lon': False},
     title='receita por estado',
+)
+
+grafico_vendas_mes = px.area(
+    data_frame=df_rec_mensal,
+    x= 'Data da Compra',
+    y= 'Preço',
+    labels='mês/ano',
+    title='Histórico de receita de vendas por mês'
 )
